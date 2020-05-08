1d87dd142d0ad8ab3e09306cacc861df002618b6
suuuum36 <lsm3191@snu.ac.kr>
2020-05-08 13:09:00 +0900

    1-django-seminar project seminar created

--- seminar/seminar/urls.py
+++ seminar/seminar/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import feedpage.views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', feedpage.views.index, name='index'), 
